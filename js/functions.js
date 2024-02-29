14b604df58019bec7eb17d783dc3a849dc46990e
Barboss <moormon@mail.ru>
2024-02-29 19:58:15 +0300

    ДЗ Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,10 @@
 const stringLengthCheck = (string = '', maxSymbols = 1) => string.length <= maxSymbols;
 
 stringLengthCheck();
+// console.log(stringLengthCheck('проверяемая строка', 20)); true
+// console.log(stringLengthCheck('проверяемая строка', 18)); true
+// console.log(stringLengthCheck('проверяемая строка', 10)); false
+
 
 // Функция Палиндром?
 const isPalindrome = (string = '') => {
@@ -17,3 +21,21 @@ const isPalindrome = (string = '') => {
 
 // console.log(isPalindrome('топот'));
 isPalindrome();
+
+
+// Дополнительное задание "Извлечение цифр из строки"
+
+const extractNumbers = (string) => {
+  let result = '';
+
+  string = string.toString();
+
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (Number.isNaN(parseInt(string[i], 10)) === false) {
+      result += string[i];
+    }
+  }
+  return result === '' ? NaN : Number(result);
+};
+
+extractNumbers();
